c1503e48231635051c239f9f7b50e7f5da279be0
ioannanoli <ioannoli@gmail.com>
2024-11-20 19:55:37 +0200

    αρχεια για διαβασμα doctors.xml

---
+++ DoctorCatalog.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// class that creates a private catalog with doctors 
+
+public class DoctorCatalog {
+    private List<Doctor> doctors;
+
+    public DoctorCatalog() {
+        this.doctors = new ArrayList<>();
+    }
+
+    // Create add a doctor to the catalog
+    public void addDoctor(String ID, String name, String speciality, List<String> availability) {
+        Doctor doctor = new Doctor(ID, name, speciality, availability); 
+        doctors.add(doctor);
+    }
+
+
+    // Retrieve the list of doctors
+    public List<Doctor> getDoctors() {
+        return new ArrayList<>(doctors); // Returns a copy 
+    }
+
+    public void printCatalog() {
+        for (Doctor doctor : doctors) {
+            System.out.println(doctor);
+        }
+    }
+}
